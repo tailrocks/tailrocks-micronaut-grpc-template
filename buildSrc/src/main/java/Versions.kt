df3d845fddece23d50fc428cfa307897f8cc83c5
Alexey Zhokhov <alexey@zhokhov.com>
2021-06-28 02:56:06 +0800

    Update dependencies.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
 
     // Languages
 
-    const val kotlin = "1.5.10"
+    const val kotlin = "1.5.20"
 
     // Gradle plugins
 
@@ -10,7 +10,7 @@ object Versions {
     const val gradleVersionsPlugin = "0.39.0"
 
     // https://plugins.gradle.org/plugin/com.diffplug.spotless
-    const val gradleSpotlessPlugin = "5.13.0"
+    const val gradleSpotlessPlugin = "5.14.0"
 
     // https://plugins.gradle.org/plugin/com.gorylenko.gradle-git-properties
     const val gradleGitPropertiesPlugin = "2.3.1"
@@ -22,23 +22,23 @@ object Versions {
     const val gradleShadowPlugin = "7.0.0"
 
     // https://plugins.gradle.org/plugin/io.micronaut.application
-    const val gradleMicronautPlugin = "1.5.1"
+    const val gradleMicronautPlugin = "2.0.1"
 
     // Libraries
 
     // REMEMBER update micronautVersion in gradle.properties as well
-    const val micronaut = "2.5.5"
+    const val micronaut = "2.5.7"
 
     // UPDATE together with Micronaut
-    // https://repo1.maven.org/maven2/io/micronaut/micronaut-bom/2.5.5/micronaut-bom-2.5.5.pom
+    // https://repo1.maven.org/maven2/io/micronaut/micronaut-bom/2.5.7/micronaut-bom-2.5.7.pom
     // https://repo1.maven.org/maven2/io/micronaut/sql/micronaut-jooq/3.4.0/micronaut-jooq-3.4.0.pom
     // https://repo1.maven.org/maven2/io/grpc/grpc-bom/1.33.1/grpc-bom-1.33.1.pom
     // https://repo1.maven.org/maven2/io/grpc/grpc-protobuf/1.33.1/grpc-protobuf-1.33.1.pom
     const val postgresql = "42.2.18"
-    const val grpc = "1.33.1"
+    const val grpc = "1.38.0"
     const val flyway = "7.7.3"
     const val jooq = "3.14.4"
-    const val protobuf = "3.14.0"
+    const val protobuf = "3.17.2"
     const val spotbugs = "4.0.3"
     // end
 
@@ -47,7 +47,7 @@ object Versions {
     const val kotest = "4.6.0"
 
     const val jambalayaChecks = "0.4.0"
-    const val jambalayaMapstructProcessor = "0.3.0"
+    const val jambalayaMapstructProcessor = "0.3.1"
     const val jambalayaMicronautMapstructProtobuf = "0.3.0"
     const val jambalayaOpentelemetry = "0.2.0"
     const val jambalayaProtobuf = "0.4.0"
