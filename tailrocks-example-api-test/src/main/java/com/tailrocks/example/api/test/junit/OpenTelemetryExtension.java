c725e63c901b2e1e4b723ce28235989cafaf47d0
Alexey Zhokhov <alexey@zhokhov.com>
2021-06-28 04:57:30 +0800

    Close span.

--- tailrocks-example-api-test/src/main/java/com/tailrocks/example/api/test/junit/OpenTelemetryExtension.java
+++ tailrocks-example-api-test/src/main/java/com/tailrocks/example/api/test/junit/OpenTelemetryExtension.java
@@ -1,56 +1,57 @@
 package com.tailrocks.example.api.test.junit;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Scope;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.System.out;
+
 /**
  * @author Alexey Zhokhov
  */
-public class OpenTelemetryExtension implements BeforeTestExecutionCallback, BeforeEachCallback, AfterEachCallback,
-        AfterAllCallback {
+public class OpenTelemetryExtension implements BeforeEachCallback, AfterEachCallback, AfterAllCallback {
 
     private final Map<String, Scope> scopes = new HashMap<>();
-
-    public OpenTelemetryExtension() {
-    }
+    private final Map<String, Span> spans = new HashMap<>();
 
     @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
+    public void beforeEach(ExtensionContext context) {
         var tracer = GlobalOpenTelemetry.get().getTracer("test");
 
-        var span = tracer
+        Span span = tracer
                 .spanBuilder(context.getRequiredTestMethod().getName())
                 .startSpan();
 
-        System.out.println(context.getRequiredTestMethod().getName() + " TRACE ID: " + span.getSpanContext().getTraceId());
+        out.println(context.getRequiredTestClass().getSimpleName() + " > " + context.getRequiredTestMethod().getName());
+        out.println("Trace ID: " + span.getSpanContext().getTraceId());
 
+        spans.put(context.getUniqueId(), span);
         scopes.put(context.getUniqueId(), span.makeCurrent());
     }
 
     @Override
-    public void afterEach(ExtensionContext context) throws Exception {
+    public void afterEach(ExtensionContext context) {
+        spans.get(context.getUniqueId()).end();
         scopes.get(context.getUniqueId()).close();
         scopes.remove(context.getUniqueId());
     }
 
     @Override
-    public void afterAll(ExtensionContext context) throws Exception {
-        for (String key : scopes.keySet()) {
-            scopes.get(key).close();
+    public void afterAll(ExtensionContext context) {
+        for (var entry : scopes.entrySet()) {
+            entry.getValue().close();
+        }
+        for (var entry : spans.entrySet()) {
+            entry.getValue().end();
         }
         scopes.clear();
     }
 
-    @Override
-    public void beforeTestExecution(ExtensionContext context) throws Exception {
-        System.out.println("XXX");
-    }
 }
