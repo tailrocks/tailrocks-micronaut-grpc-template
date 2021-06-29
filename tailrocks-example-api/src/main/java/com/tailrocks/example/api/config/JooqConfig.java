4362f5951614e22676cce37eca18797ce9a34a09
Alexey Zhokhov <alexey@zhokhov.com>
2021-06-29 17:34:10 +0800

    Cleanup.

--- tailrocks-example-api/src/main/java/com/tailrocks/example/api/config/JooqConfig.java
+++ tailrocks-example-api/src/main/java/com/tailrocks/example/api/config/JooqConfig.java
@@ -8,7 +8,6 @@ import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import org.jooq.conf.Settings;
 
-import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Factory
@@ -16,7 +15,6 @@ public class JooqConfig {
 
     @Bean
     @Singleton
-    @Named("default")
     public Settings jooqSettings() {
         return new Settings()
                 .withReturnAllOnUpdatableRecord(true)
