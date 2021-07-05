57f0f27ae4637a59b105f24b845c02b1cc809682
Alexey Zhokhov <alexey@zhokhov.com>
2021-07-05 19:40:44 +0800

    Sonatype Snapshots.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,8 @@ dependencyResolutionManagement {
         mavenLocal()
         mavenCentral()
         // uncomment if you need to use snapshot versions
-        // maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        // maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
